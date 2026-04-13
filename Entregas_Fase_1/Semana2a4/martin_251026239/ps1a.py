da65dcc46651de0ce51bf64e78057a6ecc2b29d9
martinnho <melomartin138@gmail.com>
2026-04-13 12:46:51 -0300

    exercicio ps1a.py feito

--- Entregas_Fase_1/Semana2a4/martin_251026239/ps1a.py
+++ Entregas_Fase_1/Semana2a4/martin_251026239/ps1a.py
@@ -1,3 +1,19 @@
-yearly_salary = float(input())
-portion_saved = float(input())
-cost_of_dream_home = float(input())
+yearly_salary = float(input("Enter your yearly salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+cost_of_dream_home = float(input("Enter the cost of your dream home: "))
+
+portion_down_payment = 0.25
+amount_saved = 0
+r = 0.05
+monthly_salary = yearly_salary / 12
+months = 0
+down = cost_of_dream_home * portion_down_payment
+
+while (down > amount_saved):
+    monthly_return = amount_saved * (r/12)
+    montly_save = monthly_salary * portion_saved
+    amount_saved += monthly_return + montly_save
+
+    months += 1
+
+print(f"Number of months: {months}")
