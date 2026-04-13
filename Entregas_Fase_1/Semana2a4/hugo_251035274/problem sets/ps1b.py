24b2f5946f60f73e3bd888437425f190f0cb2b00
Hugo Rosa <194843128+HugoRosa29@users.noreply.github.com>
2026-04-13 13:50:15 -0300

    Problem set 1b solved

--- Entregas_Fase_1/Semana2a4/hugo_251035274/problem sets/ps1b.py
+++ Entregas_Fase_1/Semana2a4/hugo_251035274/problem sets/ps1b.py
@@ -1,6 +1,7 @@
 yearly_salary = float(input("Qual seu salário anual? "))
 portion_saved = float(input("Qual a porção do seu salário que você poupa? "))
 coast_of_dream_home = float(input("Qual o custo da casa dos seus sonhos? "))
+semi_annual_raise = float(input("Qual a porcentagem de aumento semestral? "))
 
 portion_down_payment = 0.25
 amount_saved = 0
@@ -9,6 +10,8 @@ months = 0
 down_payment = coast_of_dream_home * portion_down_payment
 
 while amount_saved < down_payment:
+    if months % 6 == 0 and months != 0:
+        yearly_salary += yearly_salary * semi_annual_raise
     month_saved = (yearly_salary / 12) * portion_saved
     month_return = amount_saved * (r / 12)
     
