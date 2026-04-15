49b942e5d0fd5f68510dd1fee7b1578ee9612a07
Hugo Rosa <194843128+HugoRosa29@users.noreply.github.com>
2026-04-15 12:42:41 -0300

    Finger Exercises 7 e 8 resolvidos

---
+++ Entregas_Fase_1/Semana2a4/hugo_251035274/finger exercises/fe008.py
@@ -0,0 +1,14 @@
+def same_chars(s1,s2):
+    for i in s1:
+        if i not in s2:
+            return False
+    for i in s2:
+        if i not in s1:
+            return False
+    
+    return True
+
+print(same_chars("abc", "cab")) # prints True
+print(same_chars("abccc", "caaab")) # prints True
+print(same_chars("abcd", "cabaa")) # prints False
+print(same_chars("abcabc", "cabz")) # prints False
