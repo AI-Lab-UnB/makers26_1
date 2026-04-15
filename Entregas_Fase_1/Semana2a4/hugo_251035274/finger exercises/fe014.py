425c9f4fe7019aaf4eaacc970bc597d91e2fbfcb
Hugo Rosa <194843128+HugoRosa29@users.noreply.github.com>
2026-04-15 14:03:36 -0300

    Finger Exercises 14 resolvido

--- Entregas_Fase_1/Semana2a4/hugo_251035274/finger exercises/fe014.py
+++ Entregas_Fase_1/Semana2a4/hugo_251035274/finger exercises/fe014.py
@@ -4,10 +4,24 @@ def keys_with_value(aDict, target):
     for ele in aDict:
         if aDict[ele] == target:
             resp.append(ele)
+    resp.sort()
+    
+    return resp
 
+def all_positive(d):
+    resp = []
+    
+    for ele, li in d.items():
+        if sum(li) > 0:
+            resp.append(ele)
+    resp.sort()
+    
     return resp
 
 
 aDict = {1:2, 2:4, 5:2}
 target = 2   
 print(keys_with_value(aDict, target)) # prints the list [1,5]
+
+d = {5:[2,-4], 2:[1,2,3], 1:[2]}
+print(all_positive(d))   # prints the list [1, 2]
