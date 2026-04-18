1be33dbd6588af4fa8fdaf4dc5dba9229d3775f2
daisha19 <raissasilvadeoliveira191@gmail.com>
2026-04-17 21:52:29 -0300

    Problem Set_0 - Raissa232014763

---
+++ Entregas_Fase_1/Semana2a4/raissa_232014763/Problem_Set_0.py
@@ -0,0 +1,7 @@
+import numpy
+
+a = int(input("Digite um número inteiro:"))
+b = int(input("Digite outro número inteiro:"))
+
+resultado = a ** b
+print(f"O resultado de {a} elevado a {b} é: {resultado}")
