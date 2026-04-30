4eb2fdcc1d22c110e55d5bb790487f32aceab6bb
arthur_msr <arthurmsr23@gmail.com>
2026-04-30 15:50:59 -0300

    Adicionado PS03

--- Entregas_Fase_1/Semana2a4/arthurMiguel_251005865/Finger Exercises/fgex_04.py
+++ Entregas_Fase_1/Semana2a4/arthurMiguel_251005865/Finger Exercises/fgex_04.py
@@ -1,2 +1,8 @@
-N = int(input("Digite um número para a raíz cúbica ser checada: "))
-
+i = 1
+N = int(input("Escolha um número: "))
+while i**3 < N:
+    i += 1   
+    if i**3 == N:
+        print(i)
+    else:
+        print('erro')
---
+++ Entregas_Fase_1/Semana2a4/arthurMiguel_251005865/Problem Set/Problem Set 02/ps02.py
@@ -0,0 +1,30 @@
+import string
+def has_player_won(secret_word, letters_guessed):
+    contador = 0
+    for i in secret_word:
+        for j in letters_guessed:
+            if i == j:
+                contador += 1
+    if contador == len(secret_word):
+        return True
+    
+def guess_letter(secret_word):
+    progresso = ""
+
+    for i in secret_word:
+        if i in secret_word:
+            progresso += i
+        else:
+            progresso += *
+    return progresso
+
+def get_available_letters(letters_guessed):
+    alfabeto = string.ascii_letters
+    temp=""
+    for c in alfabeto:
+        if c not in alfabeto:
+            temp += c
+    return temp
+
+def hangman(secret_word,with_help):
+    print("Bem Vindo ao jogo da forca!")
