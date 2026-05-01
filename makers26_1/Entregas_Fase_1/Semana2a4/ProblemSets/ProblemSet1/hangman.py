524c38b76346ddab83bfa459ff0d45b3804caceb
Edupizzol <edupizzol16@gmail.com>
2026-05-01 18:06:57 +0000

    FEAT: Organizando o diretório do ProblemSet1 e adicionando a Hangman.py

--- makers26_1/Entregas_Fase_1/Semana2a4/ProblemSets/ProblemSet1/hangman.py
+++ makers26_1/Entregas_Fase_1/Semana2a4/ProblemSets/ProblemSet1/hangman.py
@@ -46,88 +46,87 @@ def choose_word(wordlist):
 wordlist = load_words()
 
 def has_player_won(secret_word, letters_guessed):
-    """
-    secret_word: string, the lowercase word the user is guessing
-    letters_guessed: list (of lowercase letters), the letters that have been
-        guessed so far
+    for char in secret_word:
+        if char not in letters_guessed:
+            return False;
 
-    returns: boolean, True if all the letters of secret_word are in letters_guessed,
-        False otherwise
-    """
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+    return True;
 
 
 def get_word_progress(secret_word, letters_guessed):
-    """
-    secret_word: string, the lowercase word the user is guessing
-    letters_guessed: list (of lowercase letters), the letters that have been
-        guessed so far
+    
+    result = ""
 
-    returns: string, comprised of letters and asterisks (*) that represents
-        which letters in secret_word have not been guessed so far
-    """
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+    for char in secret_word:
+        if char not in letters_guessed:
+            result+='*'
+        else:
+            result+=char
+
+    return result
 
 
 def get_available_letters(letters_guessed):
-    """
-    letters_guessed: list (of lowercase letters), the letters that have been
-        guessed so far
+    alphabet = string.ascii_lowercase
+    avilable_letters=""
 
-    returns: string, comprised of letters that represents which
-      letters have not yet been guessed. The letters should be returned in
-      alphabetical order
-    """
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+    for char in alphabet:
+        if char not in letters_guessed:
+            avilable_letters+=char
+
+    return avilable_letters
 
 
 
 def hangman(secret_word, with_help):
-    """
-    secret_word: string, the secret word to guess.
-    with_help: boolean, this enables help functionality if true.
+    
+    print("Welcome to Hangman")
+    print(f"I am thinking of a word that is {len(secret_word)} letters long")
+    guesses_left=10
+    letters_guessed=[]
 
-    Starts up an interactive game of Hangman.
+    while guesses_left>0 and not has_player_won(secret_word,letters_guessed):
 
-    * At the start of the game, let the user know how many
-      letters the secret_word contains and how many guesses they start with.
+      print(f"{guesses_left}")
+        
+      guess = input("Please guess a letter: ").lower()
 
-    * The user should start with 10 guesses.
+      if with_help and guess=='!':
+        if guesses_left<=3:
+          print("Oops you don't have enough guesses anymore")
+        else:
+          guesses_left-=3
+          for char in secret_word:
+            if char not in letters_guessed:
+              letters_guessed.append(char)
+              print(f"Help revealed the letter '{char}'")
+              print(f"Word progress: {get_word_progress(secret_word, letters_guessed)}")
+              break 
+        continue
+      
+      if not guess.isalpha() or len(guess)!=1:
+          print("Invalid Input")
+          continue
+      
+      if guess in letters_guessed:
+        print(f"Oops! You've already guessed that letter: {get_word_progress(secret_word, letters_guessed)}")        
 
-    * Before each round, you should display to the user how many guesses
-      they have left and the letters that the user has not yet guessed.
+      elif guess in secret_word:
+        letters_guessed.append(guess)
+        print(f"Good guess: {get_word_progress(secret_word, letters_guessed)}")
 
-    * Ask the user to supply one guess per round. Remember to make
-      sure that the user puts in a single letter (or help character '!'
-      for with_help functionality)
+      else:
+        letters_guessed.append(guess)
+        if guess in "aeiou":
+            guesses_left -= 2
+        else:
+            guesses_left -= 1
+        print(f"Oops! That letter is not in my word: {get_word_progress(secret_word, letters_guessed)}")
 
-    * If the user inputs an incorrect consonant, then the user loses ONE guess,
-      while if the user inputs an incorrect vowel (a, e, i, o, u),
-      then the user loses TWO guesses.
-
-    * The user should receive feedback immediately after each guess
-      about whether their guess appears in the computer's word.
-
-    * After each guess, you should display to the user the
-      partially guessed word so far.
-
-    -----------------------------------
-    with_help functionality
-    -----------------------------------
-    * If the guess is the symbol !, you should reveal to the user one of the
-      letters missing from the word at the cost of 3 guesses. If the user does
-      not have 3 guesses remaining, print a warning message. Otherwise, add
-      this letter to their guessed word and continue playing normally.
-
-    Follows the other limitations detailed in the problem write-up.
-    """
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
-
-
+    if has_player_won(secret_word, letters_guessed):
+        print("Congratulations, you won!")
+    else:
+        print(f"Sorry, you ran out of guesses. The word was {secret_word}.")
 
 # When you've completed your hangman function, scroll down to the bottom
 # of the file and uncomment the lines to test
@@ -135,9 +134,9 @@ def hangman(secret_word, with_help):
 if __name__ == "__main__":
     # To test your game, uncomment the following three lines.
 
-    # secret_word = choose_word(wordlist)
-    # with_help = False
-    # hangman(secret_word, with_help)
+    secret_word = choose_word(wordlist)
+    with_help = False
+    hangman(secret_word, with_help)
 
     # After you complete with_help functionality, change with_help to True
     # and try entering "!" as a guess!
