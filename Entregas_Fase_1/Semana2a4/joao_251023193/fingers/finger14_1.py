bc3d396d3d1a6451172e34936922f699414d104f
jvsauma <saumajoao@gmail.com>
2026-05-03 16:36:04 -0300

    Fingers concluídos

---
+++ Entregas_Fase_1/Semana2a4/joao_251023193/fingers/finger14_1.py
@@ -0,0 +1,23 @@
+def keys_with_value(aDict, target):
+    """
+    aDict: a dictionary
+    target: an integer or string
+    Assume that keys and values in aDict are integers or strings.
+    Returns a sorted list of the keys in aDict with the value target.
+    If aDict does not contain the value target, returns an empty list.
+    """
+    # Your code here
+    
+    sorted_list = []
+    for i in aDict.keys():
+        if aDict[i] == target:
+            sorted_list.append(i)
+    
+    sorted_list.sort()
+    return sorted_list
+    
+    
+# Examples:
+aDict = {1:2, 2:4, 5:2}
+target = 2
+print(keys_with_value(aDict, target)) # prints the list [1,5]
